68e87b535e1bb4e85d7a3883651f26167db57f3a
Vitali <vkuper7@gmail.com>
2021-10-19 22:14:17 +0300

    todo list

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const App = {
         return {
             title: 'Список заметок',
             placeholderString:'Введите название заметки',
-            inputValue: ''
+            inputValue: '',
+            notes: []
         }
     },
     methods: {
         inputChangeHandler({target}){
             this.inputValue = target.value
+        },
+        addNewNote(){
+            if(this.inputValue){
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            } else{
+                alert('Введите текст заметки!')
+            }
+        },
+        clearNote(value){
+            this.notes = this.notes.filter((note)=> note !== value)
         }
     }
 }
