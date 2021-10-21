c2f6717451dabb76a22485b62a27caf95a2a878c
Vitali <vkuper7@gmail.com>
2021-10-21 21:46:02 +0300

    commit

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 Vue.createApp({
     data: () => ({
         title: 'Vue 3',
-        myHtml: `<h1>Vue App</h1>`
+        myHtml: `<h2>Vue 3 App</h2>`,
+        person: {
+            firstName: 'Kupratsevich',
+            name: 'Vitali',
+            age: 26
+        },
+        items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     })
 }).mount('#app')
