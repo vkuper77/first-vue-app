c743a6df25846ef7239bb44d37d4ccad9c0198cc
Vitali <vkuper7@gmail.com>
2021-11-04 22:11:52 +0300

    finish test vue

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ Vue.createApp({
             name: 'Vitali',
             age: 26
         },
-        items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        items: [1, 2, 3, 4, 5, 6, 7, 8, 9]
     }),
+    methods:{
+        addItem(){
+            this.items.unshift(this.$refs.myInput.value)
+            this.$refs.myInput.value = ''
+        },
+        remove(idx){
+            this.items.splice(idx, 1)
+        },
+        log(idx){
+            console.log(idx)
+        }
+    },
     computed: {
         eventItems() {
             return this.items.filter(i =>
